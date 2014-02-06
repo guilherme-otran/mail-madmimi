00b2301a87aaf6db1d94b01a49937beb1d965a37
Guilherme O <guilherme_otran@hotmail.com>
2014-02-06 12:42:39 -0200

    More tests

--- spec/mail/madmimi/sender_spec.rb
+++ spec/mail/madmimi/sender_spec.rb
@@ -1,17 +1,9 @@
-require 'action_mailer'
 require 'spec_helper'
-
-class TestMailer < ActionMailer::Base
-  def test_mail
-    mail to: "somebody@example.org", promotion_name: "Promo" do |format|
-      format.html { render(text: "Oi :P") }
-    end
-  end
-end
 
 describe Mail::Madmimi::Sender do
   let(:valid_settings) { { username: "none@example.com", api_key: "1234567890" } }
   let(:mimi_sender) { described_class.new valid_settings }
+  let(:email) { TestMailer.test_mail }
 
   context "Create without api key or username" do
     it "should raise an error" do
@@ -19,11 +11,69 @@ describe Mail::Madmimi::Sender do
     end
   end
 
-  context "Promotion name aditional parameter" do
-    let(:email) { TestMailer.test_mail }
+  describe "email_post_body" do
+    context "return a hash with mimi required attributes" do
+      it "promotion_name" do
+        mimi_sender.email_post_body(email)[:promotion_name].should == "Promo"
+      end
 
-    it "email_body should return options with correct promotion_name" do
-      mimi_sender.send(:email_body, email)[:promotion_name].should == "Promo"
+      it "from" do
+        mimi_sender.email_post_body(email)[:from].should == "from@example.org"
+      end
+
+      it "recipient" do
+        mimi_sender.email_post_body(email)[:recipient].should == "somebody@example.org"
+      end
+
+      it "bcc" do
+        mimi_sender.email_post_body(email)[:bcc].should == "bcc@example.org"
+      end
+
+      it "subject" do
+        mimi_sender.email_post_body(email)[:subject].should == "Some subject"
+      end
+
+      it "raw_html" do
+        mimi_sender.email_post_body(email)[:raw_html].should == "Oi :P"
+      end
+    end
+  end
+
+  describe "parse_response" do
+    context "Received a ok response" do
+      let(:ok_reponse) { CorrectResponse.new }
+
+      it { mimi_sender.parse_response(ok_reponse).should == "1234567890" }
+    end
+
+    context "Received a non ok response" do
+      let(:invalid_response) { IncorrectResponse.new }
+
+      it "raises an error" do
+        expect { mimi_sender.parse_response(invalid_response) }
+          .to raise_error Mail::Madmimi::Sender::MadmimiError
+      end
+    end
+  end
+
+  describe "deliver!" do
+    it "posted the email" do
+      Mail::Madmimi::Sender.should_receive(:post).and_return(CorrectResponse.new)
+      mimi_sender.deliver!(email)
+    end
+
+    it "posted with correct path" do
+      Mail::Madmimi::Sender.should_receive(:post)
+        .with('/mailer', anything)
+        .and_return(CorrectResponse.new)
+      mimi_sender.deliver!(email)
+    end
+
+    it "posted with correct body" do
+      Mail::Madmimi::Sender.should_receive(:post)
+        .with(anything, body: mimi_sender.email_post_body(email))
+        .and_return(CorrectResponse.new)
+      mimi_sender.deliver!(email)
     end
   end
 end
