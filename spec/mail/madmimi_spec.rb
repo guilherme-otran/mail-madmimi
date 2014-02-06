e6b342dc0712d3abf397a12a425e578c37f49f57
Guilherme O <guilherme_otran@hotmail.com>
2014-02-06 12:42:39 -0200

    More tests

--- spec/mail/madmimi_spec.rb
+++ spec/mail/madmimi_spec.rb
@@ -1,8 +1,22 @@
-require 'action_mailer'
 require 'spec_helper'
 
 describe Mail::Madmimi, "ActionMailer load process" do
   it "should register as delivery method" do
     ActionMailer::Base.delivery_methods[:mad_mimi].should == Mail::Madmimi::Sender
+  end
+end
+
+describe Mail::Madmimi, "ActionMailer integration process" do
+  let(:email) { TestMailer.test_mail }
+  let(:valid_settings) { { username: "none@example.com", api_key: "1234567890" } }
+
+  before do
+    ActionMailer::Base.delivery_method = :mad_mimi
+    ActionMailer::Base.mad_mimi_settings = valid_settings
+  end
+
+  it "Madmimi::Sender instance should receive the mail for send it when delivered" do
+    Mail::Madmimi::Sender.any_instance.should_receive(:deliver!).with(email)
+    email.deliver
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,35 @@
+require 'action_mailer'
 require 'mail/madmimi'
+
+class TestMailer < ActionMailer::Base
+  def test_mail
+    mail(to: "somebody@example.org",
+      promotion_name: "Promo",
+      from: "from@example.org",
+      bcc: "bcc@example.org",
+      subject: "Some subject") do |format|
+        format.html { render(text: "Oi :P") }
+      end
+  end
+end
+
+class CorrectResponse
+  def headers
+    { "status" => 200 }
+  end
+
+  def parsed_response
+    # Transaction ID
+    "1234567890"
+  end
+end
+
+class IncorrectResponse
+  def headers
+    { "status" => 403 }
+  end
+
+  def parsed_response
+    "Not authorized"
+  end
+end
